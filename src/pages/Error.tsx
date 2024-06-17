4ebc07cdae0c63226f8b2776971ef508856c1acb
tompra <tomprayon@gmail.com>
2024-06-17 10:41:40 +0200

    implement rendering for 404 status

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,4 +1,21 @@
+import { Link, useRouteError } from 'react-router-dom';
+
 const Error: React.FC = (): JSX.Element => {
-    return <div>Error</div>;
+    const error = useRouteError() as { status?: number };
+
+    console.log('error', error);
+    if (error.status === 404) {
+        return (
+            <div>
+                <h1>Opsi! We have a 404</h1>
+                <p>
+                    It seems that we cannot find the page you are looking for...
+                </p>
+                <Link to={'/'}>Back home</Link>
+            </div>
+        );
+    }
+
+    return <h1>Something went wrong </h1>;
 };
 export default Error;
