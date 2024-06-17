46a1eccdfd69cc5e47f2e2683e0fcec236912d1b
tompra <tomprayon@gmail.com>
2024-06-17 13:34:53 +0200

    styling and rendering correct layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,17 @@ const Home: React.FC = (): JSX.Element => {
     return (
         <section className='home-container'>
             <Sidebar position='left' />
-            <div></div>
+            <div className='home-content'>
+                <div className='home-content__header'>
+                    <h3>THANK YOU TO BE HERE</h3>
+                </div>
+                <div className='home-content__main'>
+                    <h3>Content</h3>
+                </div>
+                <div className='home-content__footer'>
+                    <h3>STICKITHOM</h3>
+                </div>
+            </div>
             <Sidebar position='right' />
         </section>
     );
