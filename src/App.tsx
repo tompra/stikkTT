1b5242ba2940b60ea676900a13932ae5bb1edf3a
tompra <tomprayon@gmail.com>
2024-06-17 10:41:01 +0200

    implement browsing through the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,45 @@
 import React from 'react';
-import { Navbar } from './pages';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import {
+    Layout,
+    Error,
+    Home,
+    Collections,
+    Contact,
+    Aftercare,
+} from './pages/index';
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+        errorElement: <Error />,
+        children: [
+            {
+                index: true,
+                element: <Home />,
+            },
+            {
+                path: 'collections',
+                element: <Collections />,
+            },
+            {
+                path: 'contact',
+                element: <Contact />,
+            },
+            {
+                path: 'aftercare',
+                element: <Aftercare />,
+            },
+        ],
+    },
+]);
 
 const App: React.FC = (): JSX.Element => {
     return (
-        <div>
-            <Navbar />
-        </div>
+        <main>
+            <RouterProvider router={router} />
+        </main>
     );
 };
 export default App;
