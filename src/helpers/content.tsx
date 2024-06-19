8e4daa1b609ddfd693c6f224aab6f8ab16eb3f34
tompra <tomprayon@gmail.com>
2024-06-19 20:09:53 +0200

    create content file for creating dynamic content

---
+++ src/helpers/content.tsx
@@ -0,0 +1,11 @@
+export const pagesContent = {
+    home: {
+        leftSidebar: (
+            <h3 style={{ color: 'greenyellow' }}>Left Side Bar in Object</h3>
+        ),
+        rightSidebar: 'left side bar from the pages content object',
+        headerContent: 'Helloooooo',
+        mainContent: 'Content from the obj',
+        footerContent: 'Byeee',
+    },
+};
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,14 @@
 import PageLayout from '../components/PageLayout';
+import { pagesContent } from '../helpers/content';
 
 const Home: React.FC = (): JSX.Element => {
     return (
         <PageLayout
-            leftSidebarContent='sidebar left'
-            rightSidebarContent='sidebar right'
-            headerContent='THANK YOU FOR BEING HERE'
-            mainContent='Content from the props of the props'
-            footerContent='STICKITHOM'
+            leftSidebarContent={pagesContent.home.leftSidebar}
+            rightSidebarContent={pagesContent.home.rightSidebar}
+            headerContent={pagesContent.home.headerContent}
+            mainContent={pagesContent.home.mainContent}
+            footerContent={pagesContent.home.footerContent}
         />
     );
 };
