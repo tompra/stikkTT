04deb5f4a3f4fd0d0a65422ea14f1ee2437e4d5e
tompra <tomprayon@gmail.com>
2024-06-19 19:41:53 +0200

    add content props

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,13 @@
 import { SidebarProps } from '../types';
 
-const Sidebar: React.FC<SidebarProps> = ({ position }): JSX.Element => {
-    return <article className={`sidebar sidebar-${position}`}>Sidebar</article>;
+const Sidebar: React.FC<SidebarProps> = ({
+    position,
+    contentSidebar,
+}): JSX.Element => {
+    return (
+        <article className={`sidebar sidebar-${position}`}>
+            <h3>{contentSidebar}</h3>
+        </article>
+    );
 };
 export default Sidebar;
