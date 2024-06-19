9d69bf89646579ed1183a60304a6e57d01193918
tompra <tomprayon@gmail.com>
2024-06-19 20:30:53 +0200

    render image

--- src/helpers/content.tsx
+++ src/helpers/content.tsx
@@ -1,11 +1,18 @@
+import logo from '../assets/me.jpg';
 export const pagesContent = {
     home: {
         leftSidebar: (
             <h3 style={{ color: 'greenyellow' }}>Left Side Bar in Object</h3>
         ),
-        rightSidebar: 'left side bar from the pages content object',
+        rightSidebar: (
+            <h3 style={{ color: 'red' }}>Right Side Bar in Object</h3>
+        ),
         headerContent: 'Helloooooo',
-        mainContent: 'Content from the obj',
+        mainContent: (
+            <>
+                <img src={logo} alt='Me' style={{ width: '200px' }} />
+            </>
+        ),
         footerContent: 'Byeee',
     },
 };
