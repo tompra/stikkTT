8d7509584c662d04ec32577da37c9ed7a6161cfa
tompra <tomprayon@gmail.com>
2024-06-22 15:57:45 +0200

    change content footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,15 @@
 const Footer: React.FC = (): JSX.Element => {
-    return <footer className='footer-container'>Footer</footer>;
+    const year = new Date().getFullYear();
+    return (
+        <footer className='footer-container'>
+            <div>
+                <p>[IG] -- @stikkTT</p>
+                <p>[GM] -- stickithom@gmail.com</p>
+            </div>
+            <div>
+                <p>Copyright -- {new Date().getFullYear()}</p>
+            </div>
+        </footer>
+    );
 };
 export default Footer;
