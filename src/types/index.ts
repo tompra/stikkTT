993529abce96fbda2531ba567dfee3fc4a40606b
tompra <tomprayon@gmail.com>
2024-06-22 15:58:26 +0200

    add more types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,20 @@
+import React from 'react';
+
 export interface SidebarProps {
     position: string;
     contentSidebar: string;
+}
+
+export interface ContentProps {
+    header: React.ReactNode | string;
+    main: React.ReactNode | string;
+    footer: React.ReactNode | string;
+}
+
+export interface PageLayoutProps {
+    leftSidebarContent: React.ReactNode;
+    rightSidebarContent: React.ReactNode;
+    headerContent: React.ReactNode;
+    mainContent: React.ReactNode;
+    footerContent: React.ReactNode;
 }
