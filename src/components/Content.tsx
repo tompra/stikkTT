f55b0ef5e3bc019d84f5543e92c6a20ef868d50b
tompra <tomprayon@gmail.com>
2024-06-22 15:55:37 +0200

    change the content element

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import { ContentProps } from '../types';
 
-const Content: React.FC = ({ header, main, footer }): JSX.Element => {
+const Content: React.FC<ContentProps> = ({
+    header,
+    main,
+    footer,
+}): JSX.Element => {
     return (
         <div className='home-content'>
             <div className='home-content__header'>
                 <h3>{header}</h3>
             </div>
-            <div className='home-content__main'>
-                <h3>{main}</h3>
-            </div>
+            <div className='home-content__main'>{main}</div>
             <div className='home-content__footer'>
                 <h3>{footer}</h3>
             </div>
